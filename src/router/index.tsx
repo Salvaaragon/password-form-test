9d21167bbe4aca5bc61fe79f851ca6525b1cee92
Salva Aragón Reyes <salva.aragonreyes@gmail.com>
2021-03-30 15:35:00 +0200

    refactor: remove useless data

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,10 @@
 import FormView from '@Screens/FormView';
-import Manual from '@Screens/Manual';
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 const Router: React.FC = () => {
   return (
     <Switch>
-      <Route exact path="/manual" component={Manual} />
       <Route component={FormView} />
     </Switch>
   );
