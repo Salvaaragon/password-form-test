e3eed4fbc8c34b0787b56671274d14e6fac78f9a
Salva Aragón Reyes <salva.aragonreyes@gmail.com>
2021-03-30 15:35:20 +0200

    feat: update layout style, icons and firstStep images

---
+++ src/components/TextWithIcon/styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const CustomImageIcon = styled.img`
+  margin-bottom: 1rem;
+  max-height: 135px;
+`;
--- src/screens/FormView/index.tsx
+++ src/screens/FormView/index.tsx
@@ -1,4 +1,5 @@
 import LanguageSelector from '@Components/LanguageSelector';
+import { Container, Grid } from '@material-ui/core';
 import React from 'react';
 import WizardContent from './components/WizardContent';
 import WizardHeader from './components/WizardHeader';
@@ -6,14 +7,32 @@ import { FormContainerGrid, FormViewGrid, WizardFormGrid } from './styles';
 
 const FormView: React.FC = () => {
   return (
-    <FormViewGrid container justify="flex-end" alignContent="flex-start">
-      <LanguageSelector />
-      <FormContainerGrid container justify="center" alignContent="flex-start">
-        <WizardFormGrid container item xs={11} sm={8} md={8} lg={6}>
-          <WizardHeader />
-          <WizardContent />
-        </WizardFormGrid>
-      </FormContainerGrid>
+    <FormViewGrid container alignContent="flex-start">
+      <Grid item xs={12} container justify="flex-end">
+        <LanguageSelector />
+      </Grid>
+      <Grid item xs={12}>
+        <Container maxWidth="xl" disableGutters>
+          <FormContainerGrid
+            container
+            justify="center"
+            alignContent="flex-start"
+          >
+            <WizardFormGrid
+              container
+              item
+              xs={11}
+              sm={10}
+              md={10}
+              lg={8}
+              xl={6}
+            >
+              <WizardHeader />
+              <WizardContent />
+            </WizardFormGrid>
+          </FormContainerGrid>
+        </Container>
+      </Grid>
     </FormViewGrid>
   );
 };
